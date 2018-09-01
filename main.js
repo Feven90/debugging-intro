2ec79451ee35d199645ff6cc3cb18d8f6aa9f66e
Feven Mulugeta <f.mulugeta90@gmail.com>
2018-09-01 12:32:19 -0500

    added index,main.js and main.css

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('hello!');
+const colors = ["Red", "Orange", "Periwinkle", "Goldenrod", "Tomato", "Dodger Blue"]
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+}
